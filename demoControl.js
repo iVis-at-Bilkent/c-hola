1bfb4126712e172be0d1e1231451b577104ff111
Mubashira Zaman <31931663+MobiZaman@users.noreply.github.com>
2020-03-24 03:04:16 +0300

    Updated code
    Added graphs with multiple node sizes,

--- demoControl.js
+++ demoControl.js
@@ -1,25 +1,8 @@
-/**
- * JS control file for the demo
- *
- * @author Alihan Okka
- *
- * @copyright i-Vis Research Group, Bilkent University, 2007 - present
- */
-
 // Variables and Constants
-let metrics;
-let selectedEdge;
-let idealEdgeLength = 50;
-let constraints = {};
-let rotations = {};
-
 const sampleGraphs = document.getElementById("sampleGraphs");
 
-
 // Clear Selections/Options at start
-
 document.getElementById("sampleGraphs").selectedIndex = 0;
-
 
 // Initial Layout on opening the page
 var cy = window.cy = cytoscape({
@@ -29,7 +12,6 @@ var cy = window.cy = cytoscape({
             name: 'random',
             animate: false
           },
-
           style: [
             {
               selector: 'node',
@@ -91,6 +73,8 @@ var cy = window.cy = cytoscape({
                   { data: { id: 'n27', name: 'n27' } },
                   { data: { id: 'n28', name: 'n28' } },
                   { data: { id: 'n29', name: 'n29' } },
+                  { data: { id: 'n30', name: 'n30' } },
+                  { data: { id: 'n31', name: 'n31' } }
               ],
               edges: [
                   { data: { source: 'n2', target: 'n3' } },
@@ -98,7 +82,6 @@ var cy = window.cy = cytoscape({
                   { data: { source: "n3", target: "n5" } },
                   { data: { source: "n5", target: "n6" } },
                   { data: { source: 'n5', target: 'n7' } },
-                  //{ data: { source: 'n6', target: 'n7' } },
                   { data: { source: "n7", target: "n8" } },
                   { data: { source: "n8", target: "n9" } },
                   { data: { source: 'n7', target: 'n10' } },
@@ -110,7 +93,6 @@ var cy = window.cy = cytoscape({
                   { data: { source: "n12", target: "n13" } },
                   { data: { source: "n12", target: "n14" } },
                   { data: { source: "n12", target: "n15" } },
-                  //{ data: { source: "n13", target: "n15" } },
                   { data: { source: 'n15', target: 'n16' } },
                   { data: { source: 'n15', target: 'n17' } },
                   { data: { source: "n15", target: "n18" } },
@@ -120,13 +102,23 @@ var cy = window.cy = cytoscape({
                   { data: { source: "n19", target: "n21" } },
                   { data: { source: "n27", target: "n28" } },
                   { data: { source: "n28", target: "n29" } },
-                  { data: { source: "n29", target: "n27" } },
+                  { data: { source: "n29", target: "n30" } },
+                  { data: { source: "n30", target: "n31" } },
+                  { data: { source: "n31", target: "n27" } }
 
               ]
           }
       });
 
 document.getElementById("cholaLayoutButton").addEventListener("click", function(){
+  //some edge types may have been changed to segments
+  //so we convert them back to the original type haystack
+  var allEdges = cy.edges();
+  for (let i = 0; i < allEdges.length; i++)
+  {
+    allEdges[i].css("curve-style", "haystack");
+  }
+
   var layout = cy.layout({
     name: 'chola',
     animate: 'end',
@@ -139,17 +131,32 @@ document.getElementById("cholaLayoutButton").addEventListener("click", function(
 });
 
 document.getElementById("randomize").addEventListener("click", function(){
+  //some edge types may have been changed to segments
+  //so we convert them back to the original type haystack
+  var allEdges = cy.edges();
+  for (let i = 0; i < allEdges.length; i++)
+  {
+    allEdges[i].css("curve-style", "haystack");
+  }
+
   var layout = cy.layout({
     name: 'random',
     animate: true,
     animationDuration: 1000,
     animationEasing: 'ease-out'
   });
-
   layout.run();
 });
 
 document.getElementById("cose").addEventListener("click", function(){
+  //some edge types may have been changed to segments
+  //so we convert them back to the original type haystack
+  var allEdges = cy.edges();
+  for (let i = 0; i < allEdges.length; i++)
+  {
+    allEdges[i].css("curve-style", "haystack");
+  }
+
   var layout = cy.layout({
     name: 'cose-bilkent',
     animate: true,
@@ -167,9 +174,6 @@ document.getElementById("sampleGraphs").addEventListener("change",function(){
     window.cy.remove('edges');
     window.cy.remove('nodes');
 
-    constraints = {};
-    rotations = {};
-
     if( sampleGraphs.value == "1" ) {
         fetch("samples/sample1.json")
             .then(response => response.json())
@@ -181,6 +185,17 @@ document.getElementById("sampleGraphs").addEventListener("change",function(){
             .then(response => response.json())
             .then(json => { console.log(json)
               window.cy.json(json);
+              let i = 0;
+              window.cy.nodes().forEach(function(node){
+                let width = [30, 50, 70, 90, 110];
+                let size = width[i%5];
+                i++;
+                node.style({
+                  'width': size,
+                  'height': size
+                });
+              });
+
             });
     } else if( sampleGraphs.value == "3"){
         fetch("samples/sample3.json")
@@ -193,142 +208,18 @@ document.getElementById("sampleGraphs").addEventListener("change",function(){
             .then(response => response.json())
             .then(json => { console.log(json)
               window.cy.json(json);
+              let i = 0;
+              window.cy.nodes().forEach(function(node){
+                let width = [30, 50, 70, 90, 110];
+                let size = width[i%5];
+                i++;
+                node.style({
+                  'width': size,
+                  'height': size
+                });
+              });
             });
     }
 
     window.cy.endBatch();
 });
-
-// Changes arrow types
-function changeArrowShape( edge, endpoint, portConstraintType ) {
-    switch( portConstraintType ){
-        case 'Free':
-            if( endpoint == 'Source' )
-                edge.style({ 'source-arrow-shape': 'vee', 'source-arrow-color': '#930fcf' });
-            else
-                edge.style({ 'target-arrow-shape': 'vee', 'target-arrow-color': '#00c6cf' });
-            break;
-        case 'Sided':
-            if( endpoint == 'Source' )
-                edge.style({ 'source-arrow-shape': 'triangle-tee', 'source-arrow-color': '#930fcf' });
-            else
-                edge.style({ 'target-arrow-shape': 'triangle-tee', 'target-arrow-color': '#00c6cf' });
-            break;
-        case 'Absolute':
-            if( endpoint == 'Source' )
-                edge.style({ 'source-arrow-shape': 'circle', 'source-arrow-color': '#930fcf' });
-            else
-                edge.style({ 'target-arrow-shape': 'circle', 'target-arrow-color': '#00c6cf' });
-            break;
-    }
-}
-
-// Add to history
-function addToHistory( edge, endpoint, portConstraintType, portConstraintParameter ) {
-    let row = logsTable.insertRow();
-    row.onclick = function(event){ if(event.ctrlKey) deleteRowElements(row); };
-    let cell4 = row.insertCell(0);
-    let cell3 = row.insertCell(0);
-    let cell2 = row.insertCell(0);
-    let cell1 = row.insertCell(0);
-    cell1.innerHTML = edge.data('id');
-    cell2.innerHTML = endpoint;
-    cell3.innerHTML = portConstraintType;
-    cell4.innerHTML = (portConstraintParameter) ? portConstraintParameter : 'N/A';
-}
-
-// Add ports that should be free
-function addImplicitPorts( cytoEdge, nodeID ) {
-    let node = cy.nodes().filter( "[id = '" + nodeID + "']" );
-    let edges = node.connectedEdges();
-    for(let i = 0; i < edges.length; i++){
-        let edge = edges[i];
-        if(edge == cytoEdge){
-            continue;
-        }
-
-        let endpoint;
-        if( edge.source().data('id') == node.data('id'))
-            endpoint = 'Source';
-        else
-            endpoint = 'Target';
-
-        // Does this endpoint have a constraint already defined?
-        if( constraints[ edge.data('id') ] == undefined || constraints[ edge.data('id') ] == null ){
-            constraints[ edge.data('id') ] = [{
-                endpoint: endpoint,
-                portConstraintType: 'Free',
-                portConstraintParameter: 'N/A'
-            }];
-
-            addToHistory( edge, endpoint, 'Free', 'N/A' );
-            changeArrowShape( edge, endpoint, 'Free' );
-        } else {
-            let alreadyCons =  constraints[ edge.data('id') ];
-            let bool = false;
-            alreadyCons.forEach( function (cons) {
-                if(cons.endpoint == endpoint){
-                    bool = true;
-                }
-            });
-
-            // Other endpoint has a constraint
-            if(!bool){
-                constraints[ edge.data('id') ].push({
-                    endpoint: endpoint,
-                    portConstraintType: 'Free',
-                    portConstraintParameter: 'N/A'
-                });
-
-                addToHistory( edge, endpoint, 'Free', 'N/A' );
-                changeArrowShape( edge, endpoint, 'Free' );
-            }
-        }
-    }
-}
-
-// Delete Row Elements
-function deleteRowElements ( row ){
-    let edge =  cy.edges().filter( "[id = '" + row.cells[0].innerHTML + "']" );
-    let endpoint = row.cells[1].innerHTML;
-
-    if(endpoint == 'Source') {
-        edge.style({'source-arrow-shape': 'none'});
-        edge.style({'source-endpoint': 'outside-to-node'});
-    }else{
-        edge.style({'target-arrow-shape': 'none'});
-        edge.style({'target-endpoint': 'outside-to-node'});
-    }
-
-    delete constraints[row.cells[0].innerHTML];
-    logsTable.deleteRow(row.rowIndex);
-}
-
-// Node Rotation Table filler
-function fillNodeRotationTable() {
-    if( nodeRotationTable.rows.length == 1){
-        cy.nodes().forEach(function ( node ) {
-            let row = nodeRotationTable.insertRow();
-            row.onclick = function(event){ changeNodeRotation(row); };
-            let cell2 = row.insertCell(0);
-            let cell1 = row.insertCell(0);
-            cell1.innerHTML = node.data('id');
-            cell2.innerHTML = 'Yes';
-
-            rotations[node.data('id')] = true;
-        });
-    }
-}
-
-// Change Node Rotation 
-function changeNodeRotation( row ) {
-    let nodeId = row.cells[0].innerHTML;
-
-    if( rotations[nodeId] ){
-        row.cells[1].innerHTML = 'No';
-        rotations[nodeId] = false;
-    }else{
-        row.cells[1].innerHTML = 'Yes';
-        rotations[nodeId] = true;
-    }
-}
