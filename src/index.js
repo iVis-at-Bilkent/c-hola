cf337a24f18e164c8cf8242a384465a259191891
Mubashira Zaman <31931663+MobiZaman@users.noreply.github.com>
2020-12-10 06:52:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,1 @@
-const impl = require('./layout');
-
-// registers the extension on a cytoscape lib ref
-let register = function( cytoscape ){
-  if( !cytoscape ){ return; } // can't register if cytoscape unspecified
-
-  cytoscape( 'layout', 'chola', impl ); // register with cytoscape.js
-};
-
-if( typeof cytoscape !== 'undefined' ){ // expose to global cytoscape (i.e. window.cytoscape)
-  register( cytoscape );
-}
-
-module.exports = register;
+module.exports = require('./chola');
