8de1fa78d7f6466ee5a2417e657dc57286989c06
Mubashira Zaman <31931663+MobiZaman@users.noreply.github.com>
2020-12-10 06:58:45 +0300

    Update assign.js

--- src/chola/assign.js
+++ src/chola/assign.js
@@ -1,29 +1,30 @@
 const Nbr = require('../chola/nbr');
 const Arrangement = require('../chola/arrangement');
 
-function assign() {
+function assign() 
+{
 }
 
 assign.prototype.getCyclicOrder = function (node)
 {
-  var neighbors = [];
-  for (let i = 0; i < node.edges.length; i++)
-  {
-    var edge = node.edges[i];
-    var other = edge.getOtherEnd(node);
-    var otherLoc = other.getCenter();
-    var nodeLoc = node.getCenter();
-    var dx = otherLoc.x - nodeLoc.x;
-    var dy = otherLoc.y - nodeLoc.y;
-    var moved = other.moved;
-    var degree = other.getDegree();
-    var processd = other.processed;
-    var neighbor = new Nbr(other.id, dx, dy, degree, processd);
-    neighbors.push(neighbor);
-  }
-  var arr = new Arrangement(neighbors, node.getDegree());
-  arr.getArrangement();
-  return arr.getCyclicOrder();
+    var neighbors = [];
+    for (let i = 0; i < node.edges.length; i++)
+    {
+        var edge = node.edges[i];
+        var other = edge.getOtherEnd(node);
+        var otherLoc = other.getCenter();
+        var nodeLoc = node.getCenter();
+        var dx = otherLoc.x - nodeLoc.x;
+        var dy = otherLoc.y - nodeLoc.y;
+        var moved = other.moved;
+        var degree = other.getDegree();
+        var processd = other.processed;
+        var neighbor = new Nbr(other.id, dx, dy, degree, processd);
+        neighbors.push(neighbor);
+    }
+    var arr = new Arrangement(neighbors, node.getDegree());
+    arr.getArrangement();
+    return arr.getCyclicOrder();
 };
 
 
